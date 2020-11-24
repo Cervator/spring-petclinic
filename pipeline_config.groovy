b70cbd8a59e1c5cfff6694e1dcb95dc1817f331e
Cervator <cervator@gmail.com>
2020-11-24 13:17:19 -0600

    Example with no JTE files

--- pipeline_config.groovy
+++
@@ -1,7 +0,0 @@
-// restrict individual repository Jenkinsfiles if desired
-//allow_scm_jenkinsfile = false
-
-libraries {
-  //merge = false // disallow individual repos to contribute additional libraries
-  testBuild
-}
